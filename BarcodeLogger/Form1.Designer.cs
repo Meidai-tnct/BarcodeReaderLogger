b470fa3c1207f0077774c6015b84f75b1da92318
Akihiro Okita <okita.tnct@gmail.com>
2019-01-31 22:36:43 +0900

    最小限の機能で完成

--- BarcodeLogger/Form1.Designer.cs
+++ BarcodeLogger/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "読み込んでください";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 55);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(253, 195);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(191, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ファイルを開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "フォルダを開く";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 258);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(89, 19);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(278, 288);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "バーコードロガー";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- BarcodeLogger/Form1.cs
+++ BarcodeLogger/Form1.cs
@@ -12,9 +12,70 @@ namespace BarcodeLogger
 {
     public partial class Form1 : Form
     {
+        string filename;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+
+            filename = "log_" + DateTime.Today.ToString("yyyyMMdd") + ".csv";
+            textBox3.Text = filename;
+
+            try
+            {
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(filename, true))
+                {
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ファイル：" + filename + "を閉じてください。");
+            }
+
+            textBox1.Focus();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string tmp = DateTime.Now.ToString() + "," + textBox1.Text;
+                try
+                {
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(filename, true))
+                    {
+                        sw.WriteLine(tmp);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("ファイル：" + filename + "を閉じてください。");
+                }
+                textBox2.AppendText(tmp + Environment.NewLine);
+
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ファイルを開く
+            System.Diagnostics.Process.Start(filename);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // フォルダを開く
+            System.Diagnostics.Process.Start(Environment.CurrentDirectory);
         }
     }
 }
